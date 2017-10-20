10503b89019a841c2465a256933e30b7f4b64f15
Fabien Da Silva <dasilvafabien@free.fr>
2017-10-20 02:49:20 +0200

    Done project

---
+++ src/components/MovieList.vue
@@ -0,0 +1,79 @@
+<template>
+    <div id="movie-list">
+        <div v-if="filteredMovies.length">
+            <movie-item v-for="movie in filteredMovies"
+                        v-bind:movie="movie.movie">
+                <div class="movie-sessions">
+                    <div v-for="session in filteredSessions(movie.sessions)"
+                         class="session-time-wrapper tooltip-wrapper"
+                         v-tooltip="{ seats: session.seats }">
+                        <div class="session-time">{{ formatSessionTime(session.time) }}</div>
+                    </div>
+                </div>
+            </movie-item>
+        </div>
+        <div v-else-if="movies.length">
+            {{ noResults }}
+        </div>
+        <div v-else>
+            Loading...
+        </div>
+    </div>
+</template>
+
+<script>
+    import genres from '../util/genres';
+    import times from '../util/times';
+    import MovieItem from './MovieItem.vue';
+
+    export default {
+        props: ['genre', 'time', 'movies', 'day'],
+        methods: {
+            formatSessionTime(raw) {
+                return this.$moment(raw).format('h:mm A');
+            },
+            filteredSessions(sessions) {
+                return sessions.filter(this.sessionPassesTimeFilter);
+            },
+            moviesPassesGenreFilter(movie) {
+                if(!this.genre.length) {
+                    return true;
+                } else {
+                    let movieGenres = movie.movie.Genre.split(", ");
+                    let matched = true;
+                    this.genre.forEach(genre => {
+                       if(movieGenres.indexOf(genre) === -1) {
+                           matched = false;
+                       }
+                    });
+                    return matched;
+                }
+            },
+            sessionPassesTimeFilter(session) {
+                if (!this.day.isSame(this.$moment(session.time), 'day')) {
+                    return false;
+                } else if (this.time.length === 0 || this.time.length === 2) {
+                    return true;
+                } else if (this.time[0] === times.AFTER_6PM) {
+                    return this.$moment(session.time).hour() >= 18;
+                } else {
+                    return this.$moment(session.time).hour() < 18;
+                }
+            }
+        },
+        computed: {
+            filteredMovies() {
+                return this.movies
+                    .filter(this.moviesPassesGenreFilter)
+                    .filter(movie => movie.sessions.find(this.sessionPassesTimeFilter));
+            },
+            noResults() {
+                let filters = this.time.concat(this.genre).join(', ');
+                return `No Results for ${filters}.`
+            }
+        },
+        components: {
+            MovieItem
+        }
+    }
+</script>
